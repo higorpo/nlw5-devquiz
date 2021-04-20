b393df4cb2ae9b4568035a579dca8fd212a18557
Higor <higorpiresoliveira@gmail.com>
2021-04-20 11:47:51 -0300

    :sparkles: Create home screen

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,31 +15,55 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              LevelButtonWidget(
-                label: "Fácil",
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
+                  ),
+                  SizedBox(width: 8.0),
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  SizedBox(width: 8.0),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  SizedBox(width: 8.0),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
               ),
-              SizedBox(width: 8.0),
-              LevelButtonWidget(
-                label: "Médio",
-              ),
-              SizedBox(width: 8.0),
-              LevelButtonWidget(
-                label: "Difícil",
-              ),
-              SizedBox(width: 8.0),
-              LevelButtonWidget(
-                label: "Perito",
-              ),
-            ],
+            ),
           ),
-        ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              children: [
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+                QuizCardWidget(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -7,6 +7,7 @@ class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         border: Border.fromBorderSide(
           BorderSide(color: AppColors.border),
@@ -15,20 +16,32 @@ class QuizCardWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 40,
             width: 40,
             child: Image.asset(AppImages.blocks),
           ),
+          SizedBox(height: 19),
           Text("Gerenciamento de estados", style: AppTextStyles.heading15),
+          SizedBox(height: 19),
           Row(
             children: [
-              Text("3 de 10", style: AppTextStyles.body11),
-              LinearProgressIndicator(
-                value: .3,
-                backgroundColor: AppColors.chartSecondary,
-                valueColor: AlwaysStoppedAnimation(AppColors.chartPrimary),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  "3 de 10",
+                  style: AppTextStyles.body11,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: LinearProgressIndicator(
+                  value: .3,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor: AlwaysStoppedAnimation(AppColors.chartPrimary),
+                ),
               ),
             ],
           ),

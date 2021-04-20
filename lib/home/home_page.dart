72b0e2e4465ef5bd148ab8c819954780732f1674
Higor <higorpiresoliveira@gmail.com>
2021-04-20 11:27:29 -0300

    :sparkles: Create level button widget e quiz card

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,32 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LevelButtonWidget(
+                label: "Fácil",
+              ),
+              SizedBox(width: 8.0),
+              LevelButtonWidget(
+                label: "Médio",
+              ),
+              SizedBox(width: 8.0),
+              LevelButtonWidget(
+                label: "Difícil",
+              ),
+              SizedBox(width: 8.0),
+              LevelButtonWidget(
+                label: "Perito",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
